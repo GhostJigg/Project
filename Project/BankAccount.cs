eba38fef3c5f8d210fbddd13f287b2fcc1f8e112
Arsomeree <Arsomeree>
2021-03-22 15:36:30 +0000

    my account rm

---
+++ Project/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    class BankAccount
+    {
+        public string CustomerName { get; set; }
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+
+        public BankAccount(int accNo)
+        {
+            if (accNo==0)
+            {
+                accNo = 1000 + 1;
+
+            }
+
+
+            
+        }
+
+        public void Deposit (int amount)
+        {
+
+        }
+    }
+}
