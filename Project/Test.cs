044c11983d6306337a8f99896a9dcea7ac668c49
GhostJigg <Ben_wallace@hotmail.com>
2021-03-22 15:32:57 +0000

    Added Test Class

---
+++ Project/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    class Test
+    {
+        public Test()
+        {
+            Console.WriteLine("Test class!");
+        }
+    }
+}
